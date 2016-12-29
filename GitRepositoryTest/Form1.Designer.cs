968a9902eee468cc4b54e29236ddc3f0d9d05f3d
lisa <lisa.roach@cogentqc.com>
2016-12-29 10:54:02 -0800

    Test GitHub repository

--- GitRepositoryTest/Form1.Designer.cs
+++ GitRepositoryTest/Form1.Designer.cs
@@ -28,12 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.winTextBox1 = new Xceed.Editors.WinTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.winTextBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // winTextBox1
+            // 
+            this.winTextBox1.DropDownButton = null;
+            this.winTextBox1.Location = new System.Drawing.Point(56, 138);
+            this.winTextBox1.Name = "winTextBox1";
+            this.winTextBox1.Size = new System.Drawing.Size(216, 20);
+            this.winTextBox1.TabIndex = 1;
+            // 
+            // 
+            // 
+            this.winTextBox1.TextBoxArea.Name = "";
+            this.winTextBox1.TextBoxArea.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.winTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.winTextBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private Xceed.Editors.WinTextBox winTextBox1;
     }
 }
 
